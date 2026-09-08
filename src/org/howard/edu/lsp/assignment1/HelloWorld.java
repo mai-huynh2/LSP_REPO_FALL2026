bb417638da053d1996aac89b16ef9714ce1f6786
Ngoc-Mai Huynh <160189000+mai-huynh2@users.noreply.github.com>
2026-09-08 10:42:41 -0400

    Add HelloWorld class for Assignment 1

---
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,12 @@
+package org.howard.edu.lsp.assignment1;
+
+/**
+ * Prints the greeting required for Assignment 1.
+ *
+ * @author Ngoc-Mai Huynh
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
